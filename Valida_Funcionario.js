072c9c3e1b1c5378f0efa2f330ff70baa16c6010
poll-cr <yampoll-jesus@hotmail.com>
2020-12-15 17:24:00 -0600

    Add files via upload

---
+++ Valida_Funcionario.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    validaFuncionario();
+});
+
+function validaFuncionario() {
+    $("#frmFuncionario").validate({
+        rules: {
+            NombreComp: {
+                required: true,
+                maxlength: 100
+            },
+
+            Cedula: {
+                required: true,
+                maxlength: 100
+            },
+
+            provincia: {
+                required: true
+            },
+
+            canton: {
+                required: true
+            },
+
+            distrito: {
+                required: true
+            },
+            Fecha_De_Contratacion: {
+                required: true
+            }
+        }
+    });
+}
