cf3e03c7395b5e21a2055184827af6d59d08ee21
poll-cr <yampoll-jesus@hotmail.com>
2020-12-15 17:05:38 -0600

    Add files via upload

---
+++ Valida_Cursos.js
@@ -0,0 +1,21 @@
+﻿
+$(function () {
+    validarDirecciones();
+});
+
+function validarDirecciones() {
+    $("#frmCurso").validate({
+        rules: {
+            NombreCurso: {
+                required: true,
+                maxlength: 100
+            },
+
+            CodigoCurso: {
+                required: true,
+                maxlength: 100
+            }           
+
+        }
+    });
+}
